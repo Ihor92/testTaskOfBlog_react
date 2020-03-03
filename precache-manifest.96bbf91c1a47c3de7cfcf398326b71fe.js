64b6d3a18d758d8858c9839afee53c5a3343f824
Ihor92 <salfetnikov92@gmail.com>
2020-03-03 12:22:07 +0200

    Updates

---
+++ precache-manifest.96bbf91c1a47c3de7cfcf398326b71fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0113fd6f21ef422b465d3093307a1e8f",
+    "url": "/testTaskOfBlog_react/index.html"
+  },
+  {
+    "revision": "62c7f1b8e053c7b52b2b",
+    "url": "/testTaskOfBlog_react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7e1192a6afb41f1d68dc",
+    "url": "/testTaskOfBlog_react/static/js/2.7f6de039.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/testTaskOfBlog_react/static/js/2.7f6de039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62c7f1b8e053c7b52b2b",
+    "url": "/testTaskOfBlog_react/static/js/main.df54c14f.chunk.js"
+  },
+  {
+    "revision": "6cad7ff8bb90cfe33d5b",
+    "url": "/testTaskOfBlog_react/static/js/runtime-main.e19683b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testTaskOfBlog_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
